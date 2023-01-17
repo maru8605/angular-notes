16ebf8e969a43b8a0b2f2cd79ea7970b878500da
maru8605 <maru8605@hotmail.com>
2023-01-17 12:50:47 -0300

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NgStylesComponent } from './components/ng-styles/ng-styles.component';
 import { CssComponent } from './components/css/css.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStylesComponent,
     CssComponent,
-    ResaltadoDirective
+    ResaltadoDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
